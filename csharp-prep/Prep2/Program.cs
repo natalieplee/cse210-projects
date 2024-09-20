f6c015e6bb5bdf8df54ff186221d91b110b31d8a
Natalie Woster <wos22001@byui.edu>
2024-09-20 15:48:11 -0600

    made changes for the grade prep 2 assignment

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+
+        Console.Write("What is your grade percentage? ");
+        string percentage = Console.ReadLine();
+        int grade = int.Parse(percentage);
+        string letter = "";
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.Write($"Your grade: {letter}. ");
+
+        if (grade >= 70)
+        {
+            Console.Write("Congratulations, you have passed the class!");
+        }
+        else
+        {
+            Console.Write("You did not pass the class. Try a little harder next time.");
+        }
+
     }
 }
