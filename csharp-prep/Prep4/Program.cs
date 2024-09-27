2dcbde4e0c70d585737a4b8a09a2a0ddbaf607d8
Natalie Woster <wos22001@byui.edu>
2024-09-26 18:29:44 -0600

    made program that inputs given numbers into a list

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int userNumbers = -1;
+
+        while (userNumbers != 0)
+        {
+            Console.WriteLine("Enter a number, type 0 when finished: ");
+            string userResponse = Console.ReadLine();
+            userNumbers = int.Parse(userResponse);
+            numbers.Add(userNumbers);
+            
+        }
+        int sum = 0;
+        int max = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+            if (max < number)
+            {
+                max = number;
+            }
+        }
+        Console.WriteLine($"The sum of the numbers is {sum}.");
+        Console.WriteLine($"The average is {((float)sum) / numbers.Count}.");
+        Console.WriteLine($"The largest number is {max}.");
     }
 }
